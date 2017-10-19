a69c65636cf9da2d64ab733da82364b4a0734e0e
Tomasz Komisarczyk <tomekkomisarczyk@gmail.com>
2017-10-19 18:09:47 +0200

    Przerobiony tutorial 2: https://www.youtube.com/watch?v=q-XlaN-hOg8&list=PLS9MbmO_ssyB_F9AhtJulWkHBCg4Q4tTE&index=4

--- Tropikalna wyspa/DemoWyspa.cs
+++ Tropikalna wyspa/DemoWyspa.cs
@@ -5,43 +5,20 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Tropikalna_wyspa
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class DemoWyspa : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D texture;
-        Vector2 position;
+        Vector2 texturePos;
 
         public DemoWyspa()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            position = new Vector2(0, 0);
-            this.IsFixedTimeStep = true;
-            this.TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 200);
+            texturePos = new Vector2(0, 0);
         }
 
-        protected override void OnActivated(object sender, EventArgs args)
-        {
-            Window.Title = "Aktywne okno";
-            base.OnActivated(sender, args);
-        }
-
-        protected override void OnDeactivated(object sender, EventArgs args)
-        {
-            Window.Title = "Nieaktywne okno";
-            base.OnActivated(sender, args);
-        }
-
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             texture = new Texture2D(this.GraphicsDevice, 100, 100);
@@ -52,33 +29,18 @@ namespace Tropikalna_wyspa
             texture.SetData<Color>(colorData);
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            texture = this.Content.Load<Texture2D>("logo");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             texture.Dispose();
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (IsActive)
@@ -88,23 +50,23 @@ namespace Tropikalna_wyspa
                     Keys.Escape))
                     Exit();
 
-                position.X += 60.0f*(float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (position.X > this.GraphicsDevice.Viewport.Width)
-                    position.X = 0;
+                texturePos.X += 60.0f*(float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (texturePos.X > this.GraphicsDevice.Viewport.Width)
+                    texturePos.X = 0;
                 base.Update(gameTime);
             }
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(texture, position);
+            spriteBatch.Draw(texture, position: new Vector2(0.0f,0.0f), color:Color.White);
+            spriteBatch.Draw(texture,
+                destinationRectangle: new Rectangle(texture.Width, texture.Height, texture.Width, texture.Height),
+                rotation: 0f / 180f * 3.1415f,
+                origin: new Vector2(texture.Width / 2, texture.Height / 2));
             spriteBatch.End();
 
             base.Draw(gameTime);
