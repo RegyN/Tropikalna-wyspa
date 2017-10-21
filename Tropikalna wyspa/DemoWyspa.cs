408f7d79b93bf86cd1327ca9eb9cfbbe11a797f8
Tomasz Komisarczyk <tomekkomisarczyk@gmail.com>
2017-10-21 18:10:23 +0200

    Połowa samouczka #5 o grafice 3D

--- Tropikalna wyspa/DemoWyspa.cs
+++ Tropikalna wyspa/DemoWyspa.cs
@@ -9,39 +9,71 @@ namespace Tropikalna_wyspa
     public class DemoWyspa : Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        Texture2D texture;
-        Vector2 texturePos;
         KeyboardState prevState;
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix worldMatrix;
+        Matrix viewMatrix;
+
+        BasicEffect basicEffect;
+
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
+        bool orbit;
 
         public DemoWyspa()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            texturePos = new Vector2(0, 0);
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            texture = new Texture2D(this.GraphicsDevice, 100, 100);
-            Color[] colorData = new Color[100 * 100];
-            for (int i = 0; i < 10000; i++)
-                colorData[i] = Color.Red;
             prevState = Keyboard.GetState();
-            texture.SetData<Color>(colorData);
             base.Initialize();
+
+            //Setup camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45f),
+                GraphicsDevice.DisplayMode.AspectRatio, 1f, 1000f);
+
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
+
+            //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1.0f;
+            basicEffect.VertexColorEnabled = true;
+            basicEffect.LightingEnabled = false;
+
+            //Create our triangle
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(20, -20, 0), Color.Blue);
+
+            vertexBuffer = new VertexBuffer(GraphicsDevice, 
+                typeof(VertexPositionColor), 
+                3, 
+                BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
         }
         
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            texture = this.Content.Load<Texture2D>("logo");
+
         }
 
         protected override void UnloadContent()
         {
-            texture.Dispose();
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,22 +81,48 @@ namespace Tropikalna_wyspa
             if (IsActive)
             {
                 KeyboardState state = Keyboard.GetState();
-                if (state.IsKeyDown(Keys.Escape))
-                    Exit();
-                if (state.IsKeyDown(Keys.A))
-                    System.Diagnostics.Debug.WriteLine("Siema");
-                if (state.IsKeyDown(Keys.Down))
-                    texturePos.Y += 10;
-                if (state.IsKeyDown(Keys.Up))
-                    texturePos.Y -= 10;
-                if (state.IsKeyDown(Keys.Left))
-                    texturePos.X -= 10;
                 if (state.IsKeyDown(Keys.Right))
-                    texturePos.X += 10;
-
+                {
+                    camPosition.X -= 1f;
+                    camTarget.X -= 1f;
+                }
+                if (state.IsKeyDown(Keys.Left))
+                {
+                    camPosition.X += 1f;
+                    camTarget.X += 1f;
+                }
+                if (state.IsKeyDown(Keys.Down))
+                {
+                    camPosition.Y -= 1f;
+                    camTarget.Y -= 1f;
+                }
+                if (state.IsKeyDown(Keys.Up))
+                {
+                    camPosition.Y += 1f;
+                    camTarget.Y += 1f;
+                }
+                if (state.IsKeyDown(Keys.OemPlus))
+                {
+                    camPosition.Z += 1f;
+                }
+                if (state.IsKeyDown(Keys.OemMinus))
+                {
+                    camPosition.Z -= 1f;
+                }
+                if(state.IsKeyDown(Keys.Space))
+                {
+                    orbit = !orbit;
+                }
+                if(orbit)
+                {
+                    Matrix rotationMatrix = Matrix.CreateRotationY(
+                        MathHelper.ToRadians(1f));
+                    camPosition = Vector3.Transform(camPosition, rotationMatrix);
+                }
+                viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+                
                 MouseState mstate = Mouse.GetState();
-                if (mstate.RightButton == ButtonState.Pressed)
-                    Exit();
+
                 base.Update(gameTime);
                 prevState = state;
             }
@@ -72,11 +130,24 @@ namespace Tropikalna_wyspa
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
-            spriteBatch.Begin();
-            spriteBatch.Draw(texture, position: texturePos, color:Color.White);
-            spriteBatch.End();
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            // Wyłącz wycinanie tylnych ścian
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (var pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 3);
+            }
 
             base.Draw(gameTime);
         }
